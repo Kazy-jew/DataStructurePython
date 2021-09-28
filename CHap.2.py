10fc3f84747f9d3615c0a8d9e8c7eefa511e9076
Kazy-jew <cosmohieroglyphics@gmail.com>
2021-09-28 17:33:35 +0800

    commit

--- CHap.2.py
+++ CHap.2.py
@@ -1,1 +1,31 @@
 # for change
+from dategen import Calendar
+class FirstClass:
+    name = 'A'
+    sex = 'Bi'
+    def setyear(self, year):
+        self.year = year
+    def setdate(self, date):
+        self.date = date
+    def setdata(self, data):
+        self.data = data
+    def display(self):
+        print(self.data)
+
+class SecondClass(FirstClass):
+    def display(self):
+        print("Current value = {}".format(self.data))
+
+class ThirdClass(SecondClass):
+    def __init__(self, value):
+        self.data = value
+    def __add__(self, other):
+        return ThirdClass(self.data + other)
+    def __str__(self):
+        return '[ThirdClass: {}]'.format(self.data)
+    def mul(self, other):
+        self.data *= other
+
+
+a = ThirdClass('abc')
+a.mul(3)
---
+++ dategen.py
@@ -0,0 +1,7 @@
+class Calendar:
+    def __init__(self):
+        self.year = 2021
+    def setyear(self, year):
+        self.year = year
+    def display(self):
+        print(self.year)
